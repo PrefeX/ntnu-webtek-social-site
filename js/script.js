80e89319876178d730b5170c1f23c180fd63ae2e
DESKTOP-DR68S2O\randreas <andreas@sdok.no>
2017-05-24 13:56:09 +0200

    fixed the image lightbox

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(function() {
     $('[data-tooltip="tooltip"]').tooltip()
 })
 $(function() {
-		$('.pop').on('click', function() {
+		$('.lightbox').on('click', function() {
 			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
 			$('#imageModal').modal('show');   
 		});		
