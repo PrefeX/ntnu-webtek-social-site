283bf0af7fddf0c2184ba1ceb8944d5adb9e149d
DESKTOP-DR68S2O\randreas <andreas@sdok.no>
2017-05-24 13:49:57 +0200

    added lightbox for the photos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(function() {
     $('[data-tooltip="tooltip"]').tooltip()
 })
+$(function() {
+		$('.pop').on('click', function() {
+			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
+			$('#imageModal').modal('show');   
+		});		
+});
